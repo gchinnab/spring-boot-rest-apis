7083d871e981dee4cb20ff2ec2bc3dd8d7cd35ea
gchinnab <createrbrahma@gmail.com>
2026-08-18 12:49:17 +0530

    Adding entities class and mysql config.

---
+++ todos-rest/src/main/java/com/chinna/learn/springboot/todos_rest/entity/Authority.java
@@ -0,0 +1,24 @@
+package com.chinna.learn.springboot.todos_rest.entity;
+
+import jakarta.persistence.Embeddable;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+
+@Embeddable
+public class Authority implements GrantedAuthority {
+
+    private String authority;
+
+    public Authority(){
+
+    }
+
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public @Nullable String getAuthority() {
+        return authority;
+    }
+}
